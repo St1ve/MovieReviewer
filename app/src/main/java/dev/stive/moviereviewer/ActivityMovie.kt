ecb1ea2e12e31296b97fdaf83d8a4a8e40903935
razdyakonov victor <razvical@mail.ru>
2020-02-03 20:35:35 +0300

    Finished second homework without additional exercise

--- app/src/main/java/dev/stive/moviereviewer/ActivityMovie.kt
+++ app/src/main/java/dev/stive/moviereviewer/ActivityMovie.kt
@@ -9,13 +9,6 @@ import dev.stive.moviereviewer.MainActivity.Companion.MOVIE_DATA
 
 class ActivityMovie : AppCompatActivity() {
 
-    private val descriptionWhoAmIMovie =
-        "Бенджамин — молодой компьютерный гений. С детства он мечтает стать супергероем из комиксов и покорить мир. Но в реальном мире он — никто. Его жизнь неожиданно меняется, когда он встречает свою полную противоположность — харизматичного Макса. Не желая жить в жёстких рамках системы, они совершают череду дерзких кибер-преступлений. Восстав против равнодушного общества, они становятся кумирами для целого поколения. Теперь Бенджамин — не просто супергерой, он — самый разыскиваемый хакер в мире. Но что его ждёт после погружения в альтернативную реальность? Кто же он на самом деле"
-    private val descriptionAccountentMovie =
-        "Лента расскажет историю математического гения Кристиана Вульфа, который подрабатывает аудитором для самых опасных преступных организаций. Когда ему «на хвост» садится отдел по борьбе с преступностью Министерства финансов во главе с Рэем Кингом Кристиан решает найти себе законопослушного клиента и проводит аудит в компании по производству новейшей робототехники, где сотрудница финансового отдела обнаружила «нестыковочку» в миллионы долларов. Но стоит Кристиану взяться за счета и выйти на след, как в деле начинают появляться жертвы."
-    private val descriptionIronManMovie =
-        "Роберт Дауни-младший и Гвинет Пэлтроу превращаются в культовых героев знаменитых комиксов от Marvel. Тони Старк – баловень судьбы, наследник миллиардного состояния, филантроп и плейбой. У него отлично получается прожигать жизнь, он знаток лучших вечеринок, изысканных ресторанов, и в роли его спутниц всегда выступают только самые красивые девушки. Однако есть и другая грань личности героя: он руководитель огромной компании и талантливый изобретатель. Однажды в Афганистане, во время презентации одного из своих «товаров», ракеты Иерихон, Тони попадает в плен к террористам. Он тяжело ранен, но с помощью своего дара инженера конструирует невероятные доспехи, которые спасают ему жизнь и помогают выбраться из укрытия преступников. После всех своих приключений Старк намерен свернуть производство оружия в своей компании, но новоиспеченного пацифиста поддерживают далеко не все. "
-
     private lateinit var imageMovie: ImageView
     private lateinit var txtMovieTitle: TextView
     private lateinit var txtMovieDescription: TextView
@@ -42,9 +35,9 @@ class ActivityMovie : AppCompatActivity() {
         imageMovie.setImageResource(value.imgResId)
 
         when (movieName) {
-            "Who am I" -> txtMovieDescription.text = descriptionWhoAmIMovie
-            "Iron Man" -> txtMovieDescription.text = descriptionIronManMovie
-            "Accountent" -> txtMovieDescription.text = descriptionAccountentMovie
+            getString(R.string.who_am_i) -> txtMovieDescription.text = getString(R.string.description_who_am_i_movie)
+            getString(R.string.iron_man_3) -> txtMovieDescription.text = getString(R.string.description_iron_man_movie)
+            getString(R.string.accountent) -> txtMovieDescription.text = getString(R.string.description_accountent)
             else -> txtMovieDescription.text = "Description is missing"
         }
 
@@ -53,14 +46,6 @@ class ActivityMovie : AppCompatActivity() {
         intent.putExtra(MainActivity.MOVIE_COMMENT, "" )
         intent.putExtra(MainActivity.MOVIE_FAVOURITE_STATE,chkFavouriteMovie.isChecked())
         setResult(Activity.RESULT_OK, intent)
-
-//        etMovieComment.setOnFocusChangeListener { v, hasFocus ->
-//            Toast.makeText(this, "Comment", Toast.LENGTH_LONG).show()
-//            if (!hasFocus && etMovieComment == v){
-//                Toast.makeText(this, "Comment", Toast.LENGTH_LONG).show()
-//                intent.putExtra(MainActivity.MOVIE_COMMENT, etMovieComment.text)
-//            }
-//        }
 
         etMovieComment.setOnKeyListener { v, keyCode, event ->
             if (v == etMovieComment)
